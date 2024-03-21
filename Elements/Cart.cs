aedb20e8d292e57e9a95b7ae6ed5724ad39bd9df
Kevin Olaniyan <kevinolaniyan@gmail.com>
2024-03-21 12:38:37 +0100

    Add project files.

---
+++ Elements/Cart.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace QA_Assessment.Elements
+{
+    internal class Cart
+    {
+        IWebDriver WebDriver { get; }
+
+        public Cart(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+        public IWebElement TshirtDescription => WebDriver.FindElement(By.ClassName("inventory_item_desc"));
+
+        public IWebElement TshirtName => WebDriver.FindElement(By.ClassName("inventory_item_name"));
+
+        public IWebElement TshirtPrice => WebDriver.FindElement(By.ClassName("inventory_item_price"));
+
+        public IWebElement cartPage => WebDriver.FindElement(By.Id("cart_contents_container"));
+
+        public IWebElement Checkout => WebDriver.FindElement(By.Id("checkout"));
+
+        public string SelectedTshirtPrice()
+        {
+            string selectedtshritprice = TshirtPrice.Text;
+
+            return selectedtshritprice;
+
+        }
+        public string SelectedTshirtDescription()
+        {
+            string selectedtshritdescription = TshirtDescription.Text;
+
+            return selectedtshritdescription;
+
+        }
+
+        public string SelectedTshirtName()
+        {
+            string selectedtshritname = TshirtName.Text;
+
+            return selectedtshritname;
+
+        }
+    }
+}
---
+++ Elements/Products.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace QA_Assessment.Elements
+{
+    internal class Products
+    {
+        public IWebDriver WebDriver { get; }
+
+        public Products(IWebDriver driver)
+        {
+            WebDriver = driver;
+
+        }
+        public IWebElement SelectedTshirt => WebDriver.FindElement(By.CssSelector("#item_1_title_link > div"));
+
+        public IWebElement ProductContainer => WebDriver.FindElement(By.Id("inventory_container"));
+
+        public IWebElement TshirtDetailsPage => WebDriver.FindElement(By.ClassName("inventory_details"));
+
+        public IWebElement AddTshirtToCart => WebDriver.FindElement(By.ClassName("inventory_details"));
+
+        public IWebElement CartLink => WebDriver.FindElement(By.XPath("/html/body/div/div/div/div[1]/div[1]/div[3]/a"));
+
+        public string producstPageUrl()
+        {
+            return WebDriver.Url;
+        }
+
+        public void navigateToCart()
+        {
+            CartLink.Click();
+            Thread.Sleep(3000);
+        }
+
+        public string SelectedTshirtText()
+        {
+            string BoltShirt = SelectedTshirt.Text;
+
+            return BoltShirt;
+
+        }
+
+    }
+}
